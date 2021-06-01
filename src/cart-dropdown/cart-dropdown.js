ffc9352948d2e9d5d84eed6cdeda15ed68b175a1
singhkong7 <singhpriyansh2014@gmail.com>
2021-06-01 06:22:37 +0530

    Added cart-selector and made changes in the cart's dropdown

--- src/cart-dropdown/cart-dropdown.js
+++ src/cart-dropdown/cart-dropdown.js
@@ -1,11 +1,25 @@
 import React from "react";
 import CustomButton from "../custom-button/custom-button";
+import CartDescription from "../cart-description/cart-description";
+import {selectCartItems} from "../Redux/cart/cart-selector";
+import { connect } from 'react-redux';
 import "./cart-dropdown.scss";
-const CartDropdown= () =>
+const CartDropdown= ({cartItems}) =>
 (
     <div className="cart-dropdown">
-        <div className="cart-items" />
+        <div className="cart-items">
+        {
+            cartItems.map(cartItem => (
+                <CartDescription key={cartItem.id} item={cartItem} />
+            ))
+        }
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
-export default CartDropdown;
+const mapStateToProps=state=>({
+    cartItems:selectCartItems(state)
+    
+});
+  
+  export default connect(mapStateToProps)(CartDropdown);
--- src/cart/cart-item.jsx
+++ src/cart/cart-item.jsx
@@ -2,18 +2,23 @@ import React from "react";
 import {ReactComponent as ShoppingIcon} from "../cart/shopping-bag.svg";
 import {connect} from "react-redux";
 import toggleCartHidden from "../Redux/cart/cart-action";
+import {selectCartItemsCount} from "../Redux/cart/cart-selector";
 import "./cart-item.scss";
-const CartIcon= ({toggleCartHidden}) =>
+const CartIcon= ({toggleCartHidden,itemCount}) =>
 (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon" />
-        <span className="item-count">0</span>
+        <span className="item-count">{itemCount}</span>
     </div>
 );
 const mapDispatchToProps=dispatch=>({
     toggleCartHidden:()=>dispatch(toggleCartHidden())
 });
+const mapStateToProps=state=>({
+    itemCount:selectCartItemsCount(state)
+    
+});
 export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
     )(CartIcon);
